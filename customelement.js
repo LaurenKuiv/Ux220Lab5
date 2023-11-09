731a472b1e049ed21841d851b55d663ddeaa44e1
laurenkuiv <laurenkuiv@gmail.com>
2023-11-08 20:52:05 -0500

    add custom elements

--- customelement.js
+++ customelement.js
@@ -5,3 +5,19 @@ class LaurenElement extends HTMLElement{
 }
 
 customElements.define("x-lauren", LaurenElement);
+
+class CenterElement extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML = `<div style="text-align:center">${this.innerHTML}</div>`;
+    }
+}
+
+customElements.define("x-center", CenterElement);
+
+class YearElement extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML = new Date().getFullYear();
+    }
+}
+
+customElements.define("x-fullyear", YearElement);
